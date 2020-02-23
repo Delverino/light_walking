b9cf422fa19bf9810a966f171e0dc824b4c4e6a1
Ezra Szanton <ezraszanton@yahoo.com>
2020-02-22 23:34:34 -0500

    glowing spheres

---
+++ light/Assets/CharacterMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController player;
+
+    public float speed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        player.Move(move * speed * Time.deltaTime);
+
+    }
+}
---
+++ light/Assets/PlayerLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float sensitivity = 100;
+
+    public Transform body;
+
+    private float turn;
+    private float up;
+
+    private float forward;
+    private float side;
+
+    float x_rotation = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        turn = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        up = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        x_rotation -= up;
+        x_rotation = Mathf.Clamp(x_rotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(x_rotation, 0, 0);
+        body.Rotate(Vector3.up * turn);
+
+
+    }
+}
